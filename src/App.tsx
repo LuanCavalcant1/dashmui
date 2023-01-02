e0b91d3cfcbc32a0c50b76bd2dc29fa9b7e47bf0
Luan Cavalcanti <luanjcavalcanti@gmail.com>
2023-01-02 20:25:59 -0300

    feat: traduzindo o yup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 
 import { BrowserRouter } from 'react-router-dom';
+import './shared/forms/TraducoesYup';
 import { AppRoutes } from './routes';
 import { MenuLateral } from './shared/components';
 import { DrawerProvider } from './shared/contexts';
